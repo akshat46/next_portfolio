b972157c47dac389fa9c75e100a210d733192c9c
akshat46 <akshatsharma46@gmail.com>
2020-11-23 14:45:33 -0800

    Little refractor, added deploy script

--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,16 @@
-import { ChakraProvider, Text, useColorModeValue, ColorModeScript } from "@chakra-ui/core";
+import { ColorModeProvider, localStorageManager } from "@chakra-ui/core";
+import App from "../components/app";
 import customThemeLight from "../components/theme";
-import customThemeDark from "../components/theme-dark";
-import Home from "../components/home";
-import Experiences from "../components/experiences";
-import About from "../components/about";
-import Projects from "../components/projects";
-import Links from "../components/links";
-import Header from "../components/header";
-import Footer from "../components/footer";
-import Example from "../components/modules/toggle-theme";
-// import React from "react";
-import * as Scroll from "react-scroll";
-
-let Element = Scroll.Element;
 
 function Index(props) {
-    const customTheme = useColorModeValue(customThemeLight, customThemeLight);
-    const temp = useColorModeValue("red.400", "green.400");
+    // const { colorMode, toggleColorMode } = useColorMode();
+    // const customTheme = useColorModeValue(customThemeLight, customThemeLight);
+    // const temp = useColorModeValue("red.400", "green.400");
     return (
-        <ChakraProvider theme={customThemeLight}>
-            <ColorModeScript initialColorMode="light" />
-            {/* <Text color={temp}>TESTING</Text> */}
-            <Header />
-            {/* <ColorModeScript initialColorMode="light" /> */}
-            <Element name="home">
-                {/* <Example /> */}
-                <Home />
-            </Element>
-            <Element name="about">
-                <About />
-            </Element>
-            <Element name="experiences">
-                <Experiences />
-            </Element>
-            <Element name="projects">
-                <Projects />
-            </Element>
-            <Links />
-            <Footer />
-        </ChakraProvider>
+        <ColorModeProvider colorModeManager={localStorageManager} options={customThemeLight.config}>
+            <App />
+        </ColorModeProvider>
     );
 }
 
 export default Index;
-
-// class Index extends React.Component {
-//     state = {
-//         loading: true,
-//     };
-
-//     componentDidMount() {
-//         // this simulates an async action, after which the component will render the content
-//         demoAsyncCall().then(() => this.setState({ loading: false }));
-//     }
-
-//     render() {
-//         const { loading } = this.state;
-
-//         // if (loading) { // if your component doesn't have to wait for an async action, remove this block
-//         //   return (
-//         //     <Box d="block" pos="absolute" left="50%" top="50%" transform="translate(-50%)">
-//         //       <MagicSpinner size={40} color="#A6375F" loading={loading} />
-//         //     </Box>
-//         //   );
-//         // }
-
-//         return (
-//         );
-//     }
-// }
-function demoAsyncCall() {
-    return new Promise((resolve) => setTimeout(() => resolve(), 100));
-}
