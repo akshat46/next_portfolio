c36306d18f7f8868602a9b65ff8162a114fb2071
akshat46 <akshatsharma46@gmail.com>
2020-12-21 14:45:26 -0800

    fixed image size, and other bugs

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import { ColorModeProvider, localStorageManager } from "@chakra-ui/core";
-import App from "../components/app";
+import LocalApp from "../components/app";
 import customThemeLight from "../components/theme";
+import App, { Container } from "next/app";
 
 function Index(props) {
-    // const { colorMode, toggleColorMode } = useColorMode();
-    // const customTheme = useColorModeValue(customThemeLight, customThemeLight);
-    // const temp = useColorModeValue("red.400", "green.400");
     return (
         <ColorModeProvider colorModeManager={localStorageManager} options={customThemeLight.config}>
-            <App />
+            <LocalApp />
         </ColorModeProvider>
     );
 }
